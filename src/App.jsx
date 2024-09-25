91991a162dd4c73bbb10b9f99674db2b49ebd503
Daniel17971 <109083455+Daniel17971@users.noreply.github.com>
2024-09-25 12:09:19 +0100

    completed basic template for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  useEffect(() => {
+    axios.get("http://localhost:3000/todos").then((res) => {
+      setTodoList(res.data);
+    });
+  }, [setTodoList]);
 
+  const handleChange = (index, item) => {
+    setTodoList((list) => {
+      list[index]["done"] = !list[index]["done"];
+      return [...list];
+    });
+    axios.patch(`http://localhost:3000/todos/${item["id"]}`, {
+      done: !item["done"],
+    });
+  };
+  const handleClearTodos = () => {
+    setTodoList((currentList) => {
+      const newList = currentList.filter((item) => {
+        console.log(item["done"]);
+        return !item["done"];
+      });
+      todoList.forEach((item) => {
+        if (item["done"]) {
+          axios.delete(`http://localhost:3000/todos/${item["id"]}`);
+        }
+      });
+
+      return newList;
+    });
+  };
+  const handleSubmit = (e) => {
+    setTodoList((currentList) => {
+      return [...currentList, { title: newTodo, done: false }];
+    });
+    axios.post("http://localhost:3000/todos", { title: newTodo, done: false });
+  };
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>TODO List</h1>
+      <ul>
+        {todoList.map((item, index) => {
+          return (
+            <li key={item.id}>
+              {item.title}
+              <input
+                type="checkbox"
+                onChange={(e) => {
+                  handleChange(index, item);
+                }}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={handleClearTodos}>clear completed</button>
+      <h2>Add todo</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(e);
+        }}
+      >
+        <label htmlFor="addTodo">todo : </label>
+        <input
+          type="text"
+          name="addTodo"
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
